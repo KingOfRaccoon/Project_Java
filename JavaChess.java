57b6cca48f836ddcf281fb955a5e29c0c1ed06d8
Diveeva <ddiveeva@yandex.ru>
2024-05-18 14:39:07 +0300

    Add files via upload

---
+++ JavaChess.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javachess;
+
+/**
+ *
+ * @author aid
+ */
+public class JavaChess {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java Chess!");
+    }
+}
